333fe2a2f77a3ab36b67970350c4b1a7e1826cd3
Robson456 <kellherwos14@Wp.pl>
2022-12-07 06:22:10 +0100

    add logic

--- clock.js
+++ clock.js
@@ -1,3 +1,15 @@
 function clock (){
-    
+    let date = new Date();
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+
+    let time = h + ":" + m + ":" + s
+
+    document.getElementById("clock").innerText = time
+
+    var t = setTimeout(function () {clock()}, 1000);
+
 }
+
+clock();
